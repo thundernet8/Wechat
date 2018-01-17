a9520389019acf571c00e41f84f8147e28846b1e
吴学谦 <wuxueqian@ezbuy.com>
2018-01-17 20:43:52 +0800

    import basic service to officialAccount app

--- src/OfficialAccount/index.ts
+++ src/OfficialAccount/index.ts
@@ -1,12 +1,13 @@
 import ServiceContainer from "../Core/ServiceContainer";
 import BaseServiceProvider from "./Base/ServiceProvider";
 import ServerServiceProvider from "./Server/ServiceProvider";
+import MediaServiceProvider from "../BasicService/Media/ServiceProvider";
 import IAppConfig from "../Core/Interface/IAppConfig";
 
 export default class Application extends ServiceContainer {
     public constructor(config: IAppConfig) {
         super(config);
-        this.providers = [BaseServiceProvider, ServerServiceProvider];
+        this.providers = [BaseServiceProvider, ServerServiceProvider, MediaServiceProvider];
         this.initServices();
     }
 }
