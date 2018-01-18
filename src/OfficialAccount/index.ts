e5fd20d6af8662daa7267de464bfb0139ecc2b1b
吴学谦 <wuxueqian@ezbuy.com>
2018-01-18 16:26:27 +0800

    fix imports

--- src/OfficialAccount/index.ts
+++ src/OfficialAccount/index.ts
@@ -2,7 +2,7 @@ import ServiceContainer from "../Core/ServiceContainer";
 import BaseServiceProvider from "./Base/ServiceProvider";
 import ServerServiceProvider from "./Server/ServiceProvider";
 import MenuServiceProvider from "./Menu/ServiceProvider";
-import UserServiceProvider from "./Menu/ServiceProvider";
+import UserServiceProvider from "./User/ServiceProvider";
 import MediaServiceProvider from "../BasicService/Media/ServiceProvider";
 import IAppConfig from "../Core/Interface/IAppConfig";
 
