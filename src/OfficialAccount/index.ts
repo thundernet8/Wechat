9e9b2020dcc64cc72a282773b3b6773003fc631a
吴学谦 <wuxueqian@ezbuy.com>
2018-01-18 15:45:53 +0800

    register services

--- src/OfficialAccount/index.ts
+++ src/OfficialAccount/index.ts
@@ -1,13 +1,21 @@
 import ServiceContainer from "../Core/ServiceContainer";
 import BaseServiceProvider from "./Base/ServiceProvider";
 import ServerServiceProvider from "./Server/ServiceProvider";
+import MenuServiceProvider from "./Menu/ServiceProvider";
+import UserServiceProvider from "./Menu/ServiceProvider";
 import MediaServiceProvider from "../BasicService/Media/ServiceProvider";
 import IAppConfig from "../Core/Interface/IAppConfig";
 
 export default class Application extends ServiceContainer {
     public constructor(config: IAppConfig) {
         super(config);
-        this.providers = [BaseServiceProvider, ServerServiceProvider, MediaServiceProvider];
+        this.providers = [
+            BaseServiceProvider,
+            ServerServiceProvider,
+            MenuServiceProvider,
+            UserServiceProvider,
+            MediaServiceProvider
+        ];
         this.initServices();
     }
 }
