a18e9868fe2182d01e9bcc1a57cb5e7976b1972a
wuxueqian <wuxueqian2010@hotmail.com>
2018-01-21 20:42:47 +0800

    export broadcast messages

---
+++ src/Core/BroadcastMessage/index.ts
@@ -0,0 +1,15 @@
+import TextBroadcastMessage from "./TextBroadcastMessage";
+import ImageBroadcastMessage from "./ImageBroadcastMessage";
+import VoiceBroadcastMessage from "./VoiceBroadcastMessage";
+import VideoBroadcastMessage from "./VideoBroadcastMessage";
+import CardBroadcastMessage from "./CardBroadcastMessage";
+import NewsBroadcastMessage from "./NewsBroadcastMessage";
+
+export default {
+    TextBroadcastMessage,
+    ImageBroadcastMessage,
+    VoiceBroadcastMessage,
+    VideoBroadcastMessage,
+    CardBroadcastMessage,
+    NewsBroadcastMessage
+};
--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,11 @@ import OfficialAccount from "./OfficialAccount";
 import OpenPlatform from "./OpenPlatform";
 import Payment from "./Payment";
 import Reply from "./Core/Reply";
+import BroadcastMessage from "./Core/BroadcastMessage";
 
 const Core = {
-    ...Reply
+    ...Reply,
+    ...BroadcastMessage
 };
 
 const Wechat = {
